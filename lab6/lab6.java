de350a42de8777b3c1d1d931ff805c4c3e2c8199
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-10-19 17:18:33 -0700

    RE DO LAB 6 USING HASHMAP

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -1,20 +1,21 @@
 package lab6;
+import java.util.HashMap;
+
 
 public class lab6 {
   public static int firstUniqueChar(String s) {
-    int[] count = new int[26];
-
+    HashMap<Character, Integer> count = new HashMap<Character, Integer>();
     int n = s.length();
+    // build hash map : character and how often it appears
     for (int i = 0; i < n; i++) {
-      int index = s.charAt(i) - 'a';
-      count[index]++;
+      char c = s.charAt(i);
+      count.put(c, count.getOrDefault(c, 0) + 1);
     }
-    for (int i = 0; i < n; i++) {
-      int index = s.charAt(i) - 'a';
-      if (count[index] == 1) {
-        return i;
-      }
 
+    // find the index
+    for (int i = 0; i < n; i++) {
+      if (count.get(s.charAt(i)) == 1)
+        return i;
     }
     return -1;
   }
