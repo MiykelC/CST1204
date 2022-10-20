da91d90910675d55be0caf9f477ba926d75ea7db
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-10-19 17:20:26 -0700

    comments

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -6,13 +6,16 @@ public class lab6 {
   public static int firstUniqueChar(String s) {
     HashMap<Character, Integer> count = new HashMap<Character, Integer>();
     int n = s.length();
-    // build hash map : character and how often it appears
+    // building a hash map
+    // to know how many times a character appears
+    // in the string
     for (int i = 0; i < n; i++) {
       char c = s.charAt(i);
       count.put(c, count.getOrDefault(c, 0) + 1);
     }
 
-    // find the index
+    // find the index of the string
+   // where the first unique character appears
     for (int i = 0; i < n; i++) {
       if (count.get(s.charAt(i)) == 1)
         return i;
