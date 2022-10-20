6bd44fb4759bdc7b1c8cc508b101354a0b878b6f
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-10-20 12:11:21 -0700

    upd

--- Midterm/Validating HTML/src/HtmlValidator.java
+++ Midterm/Validating HTML/src/HtmlValidator.java
@@ -1,7 +1,34 @@
+
+import java.util.Queue;
+
 /*
  * This is the HtmlValidator class.
  * You should implement this class.
  */
 public class HtmlValidator {
-
+    
+    Queue<HtmlTag> aQueue;
+    
+    public HtmlValidator(){
+        Queue<HtmlTag> aQueue = new PriorityQueue();
+    }
+    public HtmlValidator(Queue<HtmlTag> tags){
+        for(HtmlTag x : tags){
+        aQueue.add(x);
+        }
+    }
+    public void addTag(HtmlTag tag){
+        aQueue.add(tag);
+    }
+    public void removeAll(String element){
+        for (HtmlTag x: tags){
+        if(x == element)
+        tags.remove(x);
+        }
+    }
+    public char[] getTags() {
+        return null;
+    }
+    public void validate() {
+    }
 }
