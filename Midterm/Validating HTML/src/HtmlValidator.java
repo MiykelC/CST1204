b009a31e2d8eaa8eab1a5c14963263331951a683
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-10-30 22:14:01 -0700

    update html

--- Midterm/Validating HTML/src/HtmlValidator.java
+++ Midterm/Validating HTML/src/HtmlValidator.java
@@ -57,33 +57,34 @@ public class HtmlValidator {
     public void validate() {
         Stack<HtmlTag> TempStack = new Stack<>();
         int indentation = 0;
-        for (HtmlTag aTag : tagsQueue) {
-            if (!aTag.isSelfClosing()) {
-                if (aTag.isOpenTag()) {
-                    // print the indentation
-                    for (int i = 0; i < indentation; i++)
-                        System.out.print(" ");
-                    System.out.println(aTag.toString());
-
-                    // you should push it onto a stack and increase your indentation.
-                    TempStack.push(aTag);
-                    indentation += 4;
+        for (HtmlTag x : tagsQueue) {
+            if (x.isOpenTag()) {
+                if (x.isSelfClosing()) {
+                    System.out.println("  ".repeat(indentation) + x);
                 } else {
-                    // print the indentation
-                    for (int i = 0; i < indentation - 4; i++)
-                        System.out.print(" ");
-                    System.out.println(aTag.toString());
+                    System.out.println("  ".repeat(indentation) + x);
+                    TempStack.push(x);
+                    indentation++;
+                }
+            } else {
+                if (TempStack.isEmpty()) {
+                    System.out.println("  ".repeat(indentation) + x);
+                    System.out.println("  ".repeat(indentation) + "ERROR: unexpected tag: " + x);
+                } else if (TempStack.peek().matches(x)) {
                     TempStack.pop();
-                    indentation -= 4;
-                } 
+                    indentation--;
+                    System.out.println("  ".repeat(indentation) + x);
+                } else {
+                    System.out.println("  ".repeat(indentation) + x);
+                    System.out.println("  ".repeat(indentation) + "ERROR: unexpected tag " + TempStack.peek());
+                }
             }
         }
-        
-        // check if the stack is not empty, print ERROR unclosed tag:
-        if (!TempStack.isEmpty()) {
-            while (!TempStack.isEmpty()) {
-                System.out.println("ERROR unclosed tag:" + TempStack.pop());
+    
+        if(!TempStack.isEmpty()) {
+            while(!TempStack.isEmpty()) {
+                System.out.println("ERROR: unclosed tag: " + TempStack.pop());
             }
         } 
-     }
- }
+    }
+}
