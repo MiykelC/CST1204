a556ece76262eb19c18519e187e694d2cc122cdd
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-11-17 09:21:14 -0800

    ,

---
+++ lab7/lab7.java
@@ -0,0 +1,39 @@
+package lab7;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class lab7 {
+
+    public static Set<Integer> getSortaSquares(Set<Integer> s) {
+       
+        Set<Integer> resultSet = new HashSet<Integer>();
+        for (Integer x : s) {
+            if (check(x)) {
+                resultSet.add(x);
+            }
+            ;
+        }
+        
+        return resultSet;
+    }
+
+    private static boolean check(Integer x) {
+        boolean result = false;
+        int n = -1;
+        int b = (int) Math.sqrt(x);
+        while (b > 0) { // my stopping criteria
+            int a = 1;
+            while (a < b) {
+                n = a b * b;
+                if (n == x) {
+                    result = true;
+                    break;
+                }
+                a++;
+            }
+            b--;
+        }
+        return result;
+    }
+}
