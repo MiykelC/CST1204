153cb41eb83374881dd9fd4fe6e0aec53d5c5419
MiykelC <105033752+MiykelC@users.noreply.github.com>
2022-12-08 10:26:16 -0800

    forking Finals part 1

---
+++ Finals/ranges/NoOverlapException.java
@@ -0,0 +1,15 @@
+package Finals.ranges;
+
+
+public class NoOverlapException extends Exception {
+
+  /**
+   * Constructor for this simple exception
+   */
+  private static final long serialVersionUID = -1673650819876266162L;
+
+  public NoOverlapException() {
+    super();
+  }
+
+}
